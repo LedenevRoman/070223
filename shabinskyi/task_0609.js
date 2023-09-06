31d9087e5650d82e51e63f5e63aad35875b67213
Vitalii.Shabinskyi <vetalsoft@gmail.com>
2023-09-06 14:08:03 +0200

    Task 2 06.09 update

--- shabinskyi/task_0609.js
+++ shabinskyi/task_0609.js
@@ -1,5 +1,39 @@
+// Вывести произвольную реакцию
 db.videos.aggregate([
     {$sample: {
       size: 1
     }}
+])
+
+// Вывести статистику по видео
+db.videos.aggregate([
+    {
+        $group: {
+          _id: null,
+          avg_duration: {
+            $avg: "$duration_secs"
+          },
+          count_videos: {
+            $count: {}
+          }
+        }
+    }
+])
+
+// Вывести самое короткое видео
+db.videos.aggregate([
+    {
+        $sort: {
+          duration_secs: 1
+        }
+    },
+    {
+        $limit: 1
+    },
+    {
+        $project: {
+          title: 1,
+          duration_secs: 1
+        }
+    }
 ])
