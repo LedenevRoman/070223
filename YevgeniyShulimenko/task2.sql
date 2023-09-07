380b2f359e3201c168805dbd9b9f1230f949e7d3
Yevgeniy_Shulimenko <evgeniy.shulimenko@gmail.com>
2023-09-07 09:30:13 +0200

    delete folder

--- YevgeniyShulimenko/task2.sql
+++
@@ -1,9 +0,0 @@
-{
-    "_id": 1,
-    "author_id": 1,
-    "created_at": {"$date": "2023-08-30T11:37:54.921Z"},
-    "duration_secs": 250,
-    "tags": "",
-    "title": "First",
-    "views": 25
-  }
--- YevgeniyShulimenko/task3.sql
+++
@@ -1,9 +0,0 @@
-db.videos.aggregate([
-    {$sort: {duration_secs: 1}},
-    {$limit: 1},
-    {$project: {
-        _id: 0,
-        title: 1,
-            duration_secs: 1
-        }}
-])
